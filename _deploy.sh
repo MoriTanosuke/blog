18ebe1d5bcbd1b976145a19265dbbf4d29ac4a4b
Carsten <carsten@kopis.de>
2011-07-27 18:43:48 +0100

    Add post about Bastion for xbox360.

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-BUCKET=blog.kopis.de
 
-s3cmd sync --delete-removed _site/ s3://${BUCKET}
+/var/lib/gems/1.8/bin/jekyll --no-auto && git push && s3cmd sync --add-header "Cache-Control: max-age=2678400, public" --skip-existing --delete-removed _site/ s3://blog.kopis.de
