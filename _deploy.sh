e6d4f85abc64cd3f23542c53b69a7b40586c0df5
Carsten <carsten@kopis.de>
2011-07-30 10:42:45 +0100

    Add +1 async code. Again.

--- _deploy.sh
+++ _deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/var/lib/gems/1.8/bin/jekyll --no-auto && git push && s3cmd sync --add-header "Cache-Control: max-age=2678400, public" --skip-existing --delete-removed _site/ s3://blog.kopis.de
+/var/lib/gems/1.8/bin/jekyll --no-auto && git push && s3cmd sync --add-header "Cache-Control: max-age=86400, public" --skip-existing --delete-removed _site/ s3://blog.kopis.de
