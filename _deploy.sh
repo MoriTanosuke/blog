dcc8cf0c755329254c78272712f5c82863ac5e89
Carsten <carsten@kopis.de>
2011-12-11 11:36:51 +0100

    Rename base-url to url

--- _deploy.sh
+++ _deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-rm -rf _site && jekyll --no-server --no-auto && rsync -vrz _site/* kopis:/kunden/191922_26384/blog/
+rm -rf _site
+jekyll --no-server --no-auto
+rsync -irz --progress _site/* kopis:/kunden/191922_26384/blog/
+
